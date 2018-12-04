b3f584582dde18d45f2c049c09c7087614557c45
adamelid <adamelid16@gmail.com>
2018-12-04 20:01:19 +0000

    Update PaymentInfo.py
    Finished class paymentInfo

--- Models/PaymentInfo.py
+++ Models/PaymentInfo.py
@@ -1,1 +1,20 @@
-#I am a comment yay
+class PaymentInfo:
+    def __init__(self, card_number, card_company, amount):
+        self.__card_number = card_number
+        self.__card_company = card_company
+        self.__amount = amount
+
+    def __str__(self):
+        return "Card number: {}, Card company: {}, Amount: {}".format(self.__card_number, self.__card_company, self.__amount)
+    
+    def getCardNumber(self):
+        return self.__card_number
+
+    def getCardCompany(self):
+        return self.__card_company
+
+    def getAmount(self):
+        return self.__amount
+    
+    def setAmount(self, new_amount):
+        self.__amount = new_amount
