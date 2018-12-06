e4b67f3226642ce4d65a2ba09c92936aee106827
magga98 <42964951+magga98@users.noreply.github.com>
2018-12-06 15:01:07 +0000

    Update CustomerService.py

--- ServiceLayer/CustomerService.py
+++ ServiceLayer/CustomerService.py
@@ -1,1 +1,20 @@
 # This is the CustomerService class
+
+from RepositoryLayer.CustomerRepository import CustomerRepository
+
+class CostumerService:
+    def __init__(self):
+        self.__customer_repo = CustomerRepository()
+
+    def addCustomer(self, customer):
+        if self.is_valid_customer(customer):
+            self.__customer_repo.add_customer(customer)
+    
+    def isValidCustomer(self, customer):
+        #Setja inn kóða
+        return True
+    
+    def getCustomer(self):
+        return self.__customer_repo.get_customer()
+    
+    
