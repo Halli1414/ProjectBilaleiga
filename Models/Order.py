e4fb9b4a4584a58578900ebe484a88e8f2e4187a
Halldor Thorri <halli1414@gmail.com>
2018-12-06 13:26:15 +0000

    Order.py finised for now

--- Models/Order.py
+++ Models/Order.py
@@ -1,11 +1,14 @@
 class Order(object):
 
-    def __init__(self, id, customer, vehicle, order_start_date, order_end_date):
+    def __init__(
+        self, id, customer, vehicle, order_start_date, order_end_date, payment
+        ):
         self.__id = id
         self.__customer = customer
         self.__vehicle = vehicle
         self.__order_start = order_start_date
         self.__order_end = order_end_date
+        self.__payment = payment
 
     def getID(self):
         return self.__id
@@ -22,6 +25,9 @@ class Order(object):
     def getOrderEndDate(self):
         return self.__order_end
 
+    def getPayment(self):
+        return self.__payment
+
     def setCustomer(self, new_customer):
         self.__customer = new_customer
 
@@ -33,3 +39,11 @@ class Order(object):
 
     def setOrderEndDate(self, new_end_date):
         self.__order_end = new_end_date
+
+    def setPayment(self, new_payment):
+        self.__payment = new_payment
+
+    def __str__(self):
+        return "Order ID: {} Customer: {} Vehicle: {} Order Start: {} Order end: {} Payment {}".format(
+            self.__id, self.__customer, self.__vehicle, self.__order_start, self.__order_end, self.__payment
+            )
