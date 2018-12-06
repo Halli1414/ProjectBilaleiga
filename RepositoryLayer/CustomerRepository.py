be2d6917b032d388622a99c2fbd8b91e096d4496
magga98 <42964951+magga98@users.noreply.github.com>
2018-12-06 13:49:04 +0000

    hjkh

--- RepositoryLayer/CustomerRepository.py
+++ RepositoryLayer/CustomerRepository.py
@@ -1,1 +1,26 @@
 # This is the CustomerRepository class
+from models.Costumer import Costumer
+
+class CostumerRepository:
+    
+    def __init__(self):
+        self.__costumer = []
+
+    def add_costumer(self, costumer):
+        with open("/data/costumer.txt","a+") as costumer_file:
+            name = costumer.get_name()
+            id = costumer.get_id()
+            phone = costumer.get_phone()
+            address = address.get_address()
+            email = email.get_email()
+            costumer_file.write("{},{},{},{},{}\n".format(name, id, phone, address, email))
+    
+    def get_costumer(self):
+        if self.__costumer == []:
+            with open("./data/costumer.txt", "r") as costumer_file:
+                for line in costumer_file.readlines():
+                    name, id, phone, address, email = line.split(",")
+                    new_costumer = costumer(name, id, phone, address, email)
+                    self.__costumer.append(new_costumer)
+        
+        return self.__costumer
