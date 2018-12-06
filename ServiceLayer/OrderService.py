6166fe2877cbf4805c22e0402c2fe4f73e5c3b19
Halldor Thorri <halli1414@gmail.com>
2018-12-06 14:40:35 +0000

    implemented OrderService.py

--- ServiceLayer/OrderService.py
+++ ServiceLayer/OrderService.py
@@ -1,1 +1,13 @@
-# this is the OrderService class
+from RepositoryLayer.OrderRepository import OrderRepository
+# This is the OrderService class
+
+class OrderService(object):
+
+    def __init__(self):
+        self.__order_repo = OrderRepository()
+
+    def addOrder(self, order):
+        self.__order_repo.addOrder(order)
+
+    def getOrders(self):
+        return self.__order_repo.getOrders()
