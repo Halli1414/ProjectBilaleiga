d6d027f80befc6605e22fecf02a29bc2d42ad2a1
Halldor Thorri <halli1414@gmail.com>
2018-12-07 12:47:09 +0000

    Minor spelling fix

---
+++ Models/Vehicle.py
@@ -0,0 +1,26 @@
+
+class Vehicle:
+    def __init__(
+        self, manufacturer, model, id, color, kilometers,  vehicleStatus
+        ):
+        self.__manufacturer = manufacturer
+        self.__model = model
+        self.__id = id
+        self.__color = color
+        self.__kilometers = kilometers
+        self.__vehicleStatus = vehicleStatus
+    
+    def getId(self):
+        return self.__id
+    def getManufacturer(self):
+        return self.__manufacturer
+    def getModel(self):
+        return self.__model
+    def getColor(self):
+        return self.__color
+    def getKilometers(self):
+        return self.__kilometers
+    def setKilometers(self):
+        return self.__kilometers
+    def setVehicleStatus(self):
+        return self.__vehicleStatus
