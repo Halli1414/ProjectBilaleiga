d71baa55749e0c24bf869c36621b954622088f96
adamelid <adamelid16@gmail.com>
2018-12-09 17:55:00 +0000

    Update MainMenuClass.py
    Added a bunch of stuff

--- UILayer/MainMenuClass.py
+++ UILayer/MainMenuClass.py
@@ -18,11 +18,48 @@ class MainMenu:
                 self.printOrderMenu()
                 self.__choice = self.getInput()
                 if self.__choice.lower() == "a":
-                    #runNewOrderFunction
+                    #run new order function here.
                 elif self.__choice.lower() == "b":
-                    #runFindOrderFunction
+                    #run find order function here.
+                    printFindOrderMenu()
+                    self.__choice = self.getInput()
+                    if self.__choice.lower() == "d":
+                        #run update order function here.
+                    if self.__choice.lower() == "e":
+                        #run delete order function here.
                 elif self.__choice.lower() == "c":
-                    #runAllOrdersFunction
+                    #run all orders function here.
+            elif self.__choice == "2":
+                self.printCustomerMenu()
+                self.__choice = self.getInput()
+                if self.__choice.lower() == "a":
+                    #run new customer function here.
+                elif self.__choice.lower() == "b":
+                    #run find customer function here.
+                    printFindCustomerMenu()
+                    self.__choice = self.getInput()
+                    if self.__choice.lower() == "d":
+                        #run update customer function here.
+                    elif self.__choice.lower() == "e":
+                        #run delete customer function here.
+                elif self.__choice.lower() == "c":
+                    #run all customers function here.
+            elif self.__choice == "3":
+                self.printVehicleMenu()
+                self.__choice = self.getInput()
+                if self.__choice.lower() == "a":
+                    #run new vehicle function here.
+                elif self.__choice.lower() == "b":
+                    #run find vehicle function here.
+                    printFindVehicleMenu()
+                    self.__choice = self.getInput()
+                    if self.__choice.lower() == "d":
+                        #run update vehicle function here.
+                    elif self.__choice.lower() == "e":
+                        #run delete vehicle function here.
+                elif self.__choice.lower() == "c":
+                    #run all vehicles function here.
+
     def getInput(self, prompt=""):
         return input(prompt)
 
@@ -39,20 +76,34 @@ class MainMenu:
         print("(3) Vehicles")
 
     def printOrderMenu(self):
+        printMainMenu()
         print("-- (a) New Order.")
         print("-- (b) Find Order.")
         print("-- (c) All Orders.")
     
     def printFindOrderMenu(self):
+        printOrderMenu()
         print("---- (d) Update Order.")
         print("---- (e) Delete Order")
     
     def printCustomerMenu(self):
+        printMainMenu()
         print("-- (a) New Customer.")
         print("-- (b) Find Customer.")
         print("-- (c) All Customers.")
 
+    def printFindCustomerMenu(self):
+        printCustomerMenu()
+        print("---- (d) Update Customer.")
+        print("---- (e) Delete Customer.")
+
     def printVehicleMenu(self):
-        print("--- (a) New Vehicle.")
-        print("--- (b) Find Vehicle.")
-        print("--- (c) All Vehicle.")
+        printMainMenu()
+        print("-- (a) New Vehicle.")
+        print("-- (b) Find Vehicle.")
+        print("-- (c) All Vehicles.")
+
+    def printFindVehicleMenu(self):
+        printVehicleMenu()
+        print("---- (d) Update Vehicle.")
+        print("---- (e) Delete Vehicle.")
