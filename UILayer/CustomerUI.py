209e38e008980b128734e117baadb09740c2131e
magga98 <42964951+magga98@users.noreply.github.com>
2018-12-11 15:40:03 +0000

    Update CustomerUI.py

--- UILayer/CustomerUI.py
+++ UILayer/CustomerUI.py
@@ -5,9 +5,16 @@ class CustomerUI:
 
     def __init__(self):
         self.__CustomerService = CustomerService()
+        self.__choice = ""
+
+    def start(self):
+        while coice != "q":
+            self._PrintMenu
+            self._NewCustomer
+            self._FindCustomer
+            self._AllCustomer
 
     def PrintMenu(self):
-
         print("1. New Customer ")
         print("2. Find Customer ")
         print("3. All customer ")
@@ -23,4 +30,12 @@ class CustomerUI:
 
         NewCustomer = Customer(name, customer_id, phone, address, email)
         self.__CustomerService.AddCustomer(NewCustomer)
- 
+    
+    def FindCustomer(self):
+        customer_id = input("ID: ")
+
+        self.__CustomerService.FindCustomer(customer_id)
+
+    def AllCustomer(self):
+        customers = self.__CustomerService.GetCustomer()
+  
