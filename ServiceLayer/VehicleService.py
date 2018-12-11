9f4a8c2cf8f6477d64fcc28c6bb54e9c397cb152
völli <volundurisar@hotmail.com>
2018-12-11 17:02:47 +0000

    Update VehicleService.py

--- ServiceLayer/VehicleService.py
+++ ServiceLayer/VehicleService.py
@@ -1,14 +1,27 @@
-from RepositoryLayer.VehicleRepository import VehicleRepository
 # this is the VehicleService class
 
 class VehicleService(object):
     def __init__(self):
         self.__vehicle_repo = VehicleRepository()
+        self.__vehicles = []
 
-    def add_Vehicle(self, Vehicle):
-        self.__vehicle_repo.addVehicle(Vehicle)
+    def addVehicle(self, Vehicle):
+        self.__vehicle_repo.add_Vehicle(Vehicle)
     
-    def get_Vehicle(self):
-        return self.__vehicle_repo.getVehicle()
+    def getVehicle(self):
+        return self.__vehicle_repo.get_Vehicle()
 
+    def findAvailable(self, VehicleStatus):
+        return_vehicle = None
+        for vehicle in self.__vehicles:
+            if vehicle.getStatus() == VehicleStatus:
+                return_vehicle = Vehicle
+        return return_vehicle
+    
+    def findUnavailable(self, VehicleStatus):
+        return_vehicle = None
+        for vehicle in self.__vehicles:
+            if vehicle.getStatus() == VehicleStatus:
+                return_vehicle = Vehicle
+        return return_vehicle
 
